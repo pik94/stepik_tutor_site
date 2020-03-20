41c7a1e14e7f7c8fccbdf5795bff7685b47ebd87
pik94 <pinchuk.ilia.work@gmail.com>
2020-03-20 16:52:42 +0300

    Added a running script and a missing script of getting json

---
+++ app.py
@@ -0,0 +1,5 @@
+from tutor_site import app
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ init_db.py
@@ -0,0 +1,27 @@
+import json
+
+from tutor_site.config import DB_PATH
+from tutor_site.data import teachers
+
+
+def main() -> None:
+    data = {
+        teacher['id']: {
+            'id':       teacher['id'],
+            'name':     teacher['name'],
+            'about':    teacher['about'],
+            'rating':   teacher['rating'],
+            'picture':  teacher['picture'],
+            'price':    teacher['price'],
+            'goals':    teacher['goals'],
+            'free':     teacher['free'],
+        }
+        for teacher in teachers
+    }
+
+    with open(DB_PATH, 'w') as file:
+        json.dump(data, file, indent=4)
+
+
+if __name__ == '__main__':
+    main()
